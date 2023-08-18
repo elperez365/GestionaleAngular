5b712748b165d6da4379081bd2f356126eb8d1a2
Stage23 <youremail@yourdomain.com>
2023-08-18 18:19:10 +0200

    .

--- src/app/pages/pages.routing.module.ts
+++ src/app/pages/pages.routing.module.ts
@@ -2,15 +2,6 @@ import { Routes } from '@angular/router';
 import { WorkersComponent } from './apps/workers/workers.component';
 
 
-export const PagesRoutes: Routes = [
-  {
-    path: '',
-    component: WorkersComponent,
-    data: {
-      title: 'Starter Page',
-    },
-  },
-
 
 export const PagesRoutes: Routes = [
   // {
