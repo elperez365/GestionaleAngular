f404a8416355402e9c0eaca035edf80e151a18c2
stage.22 <youremail@yourdomain.com>
2023-08-18 15:42:39 +0200

    Primo commit

---
+++ src/app/pages/authentication/lockscreen/lockscreen.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { CoreService } from 'src/app/services/core.service';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { MaterialModule } from '../../../material.module';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-lockscreen',
+  standalone: true,
+  imports: [
+    RouterModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf,
+  ],
+  templateUrl: './lockscreen.component.html',
+})
+export class AppLockscreenComponent {
+  options = this.settings.getOptions();
+
+  constructor(private settings: CoreService, private router: Router) {}
+
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required]),
+  });
+
+  get f() {
+    return this.form.controls;
+  }
+
+  submit() {
+    // console.log(this.form.value);
+    this.router.navigate(['/dashboards/dashboard1']);
+  }
+}
