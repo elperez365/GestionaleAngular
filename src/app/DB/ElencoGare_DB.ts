3f1f964c6de3d918afb75b684bd9d003948b21ed
elperez365 <ceti.vasco@gmail.com>
2023-08-23 09:09:38 +0200

    .

--- src/app/DB/ElencoGare_DB.ts
+++ src/app/DB/ElencoGare_DB.ts
@@ -8,12 +8,12 @@ export let Gare: ElencoGare[] = [
     ogetto: 'Manutenzione delle strade di stazione Tiburtina',
     importo: '120.000€',
     scadenza: '31-12-2023',
-    ora:"15:00",
+    ora: '15:00',
     apertura: '31-12-2023',
     criterio_aggiudicazione: 'Minor Prezzo (ART.97)',
     categoria: 'OG3 III-BIS OS12-A',
     inviata: '1',
-    ribasso:"33%",
+    ribasso: '33%',
   },
   {
     id: '2',
@@ -22,12 +22,12 @@ export let Gare: ElencoGare[] = [
     ogetto: 'Manutenzione delle strade di stazione Termini',
     importo: '250.000€',
     scadenza: '31-12-2023',
-    ora:"15:00",
+    ora: '15:00',
     apertura: '31-12-2023',
     criterio_aggiudicazione: 'Minor Prezzo (ART.97)',
     categoria: 'OG3 III-BIS OS15-A',
     inviata: '1',
-    ribasso:"33%",
+    ribasso: '33%',
   },
   {
     id: '3',
@@ -36,19 +36,18 @@ export let Gare: ElencoGare[] = [
     ogetto: 'Manutenzione delle strade di stazione San Pietro',
     importo: '170.000€',
     scadenza: '31-12-2023',
-    ora:"15:00",
+    ora: '15:00',
     apertura: '31-12-2023',
     criterio_aggiudicazione: 'Minor Prezzo (ART.97)',
     categoria: 'OG3 III-BIS OB42-A',
     inviata: '1',
-    ribasso:"33%",
+    ribasso: '33%',
   },
 ];
 
 export const GareTableHeaders: string[] = [
   'Info',
   'Stazione appaltante',
-  'Ogetto',
+  'Oggetto',
   'Importo',
-
 ];
