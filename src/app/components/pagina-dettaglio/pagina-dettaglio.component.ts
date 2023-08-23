ca2c04297b0a9d1e14743358114fc0f071fea8b9
elperez365 <ceti.vasco@gmail.com>
2023-08-23 12:46:58 +0200

    pagina dettaglio dinamica init

--- src/app/components/pagina-dettaglio/pagina-dettaglio.component.ts
+++ src/app/components/pagina-dettaglio/pagina-dettaglio.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Gare } from 'src/app/DB/ElencoGare_DB';
 
 @Component({
   selector: 'app-pagina-dettaglio',
   templateUrl: './pagina-dettaglio.component.html',
-  styleUrls: ['./pagina-dettaglio.component.scss']
+  styleUrls: ['./pagina-dettaglio.component.scss'],
 })
 export class PaginaDettaglioComponent {
+  product = Gare[1];
 
+  entriesProduct = Object.entries(this.product);
+
+  sbam() {
+    console.log(Object.values(this.product));
+  }
 }
