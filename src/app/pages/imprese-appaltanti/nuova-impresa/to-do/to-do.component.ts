9f7528dbbafba70c7e9c6b1bbbcbb1c46e0320be
elperez365 <ceti.vasco@gmail.com>
2023-08-29 13:35:23 +0200

    altri contatti 3 value ok e fix edit

--- src/app/pages/imprese-appaltanti/nuova-impresa/to-do/to-do.component.ts
+++ src/app/pages/imprese-appaltanti/nuova-impresa/to-do/to-do.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input } from '@angular/core';
-import { UntypedFormGroup, UntypedFormBuilder } from '@angular/forms';
+import {
+  UntypedFormGroup,
+  UntypedFormBuilder,
+  FormGroup,
+  FormControl,
+} from '@angular/forms';
 import { ToDo } from './todo';
 import { TodoService } from './todo.service';
 
@@ -16,21 +21,14 @@ export class ToDoComponent {
   todoId = 6;
   copyTodos: ToDo[];
   selectedCategory = 'all';
-  newName: string | null = null;
-  newTel: string | null = null;
-  newMail: string | null = null;
   editSave = 'Edit';
-  isDisabled: Boolean | (() => Boolean) =
-    !this.newTel && !this.newName && !this.newMail;
+  isDisabled: Boolean = false;
   todos: ToDo[];
+  impreseForm: FormGroup;
   // = this.todoService.getTodos();
 
   constructor(public fb: UntypedFormBuilder, public todoService: TodoService) {
     this.copyTodos = this.todos;
-  }
-
-  isOver(): boolean {
-    return window.matchMedia(`(max-width: 960px)`).matches;
   }
 
   mobileSidebar(): void {
@@ -38,6 +36,11 @@ export class ToDoComponent {
   }
 
   ngOnInit(): void {
+    this.impreseForm = new FormGroup({
+      newName: new FormControl(''),
+      newTel: new FormControl(''),
+      newEmail: new FormControl(''),
+    });
     this.todoMod
       ? (this.todos = this.todoMod)
       : (this.todos = this.todoService.getTodos());
@@ -50,7 +53,7 @@ export class ToDoComponent {
   }
 
   ngDoCheck(): void {
-    console.log(this.inputFg);
+    console.log(this.impreseForm.value);
     // this.isDisabled = () => {
     //   const newName = this.inputFg.get('newName');
     //   const newTel = this.inputFg.get('newTel');
@@ -61,13 +64,18 @@ export class ToDoComponent {
     // };
   }
 
-  addTodo(value: string): void {
-    if (this.inputFg?.get('mess')?.value.trim().length === 0) {
-      return;
-    }
+  addTodo(): void {
+    // if (this.inputFg?.get('mess')?.value.trim().length === 0) {
+    //   return;
+    // }
     this.todos.splice(0, 0, {
       id: this.todoId,
-      message: this.inputFg?.get('mess')?.value,
+      message:
+        this.impreseForm.value.newName +
+        ' - ' +
+        this.impreseForm.value.newTel +
+        ' - ' +
+        this.impreseForm.value.newEmail,
       completionStatus: false,
       edit: false,
       date: new Date(),
@@ -77,27 +85,6 @@ export class ToDoComponent {
     this.inputFg.patchValue({
       mess: '',
     });
-  }
-
-  allTodos(): void {
-    // tslint:disable-next-line - Disables all
-    this.todos.forEach(
-      (todo) =>
-        (todo.completionStatus = (<HTMLInputElement>event!.target).checked)
-    );
-  }
-
-  selectionlblClick(val: string): void {
-    if (val === 'all') {
-      this.copyTodos = this.todos;
-      this.selectedCategory = 'all';
-    } else if (val === 'uncomplete') {
-      this.copyTodos = this.todos.filter((todo) => !todo.completionStatus);
-      this.selectedCategory = 'uncomplete';
-    } else if (val === 'complete') {
-      this.copyTodos = this.todos.filter((x) => x.completionStatus);
-      this.selectedCategory = 'complete';
-    }
   }
 
   editTodo(i: number, str: string): void {
